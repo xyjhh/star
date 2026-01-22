26fcf4de93521a38fb27978e80fc0f746667d3f7
xu yonjing <2365653160@qq.com>
2026-01-22 15:12:52 +0800

    refactor(scene): 移除环境光配置
    移除了场景中对CONFIG的依赖以及环境光的添加逻辑，
    简化了createScene函数的实现。

--- src/scenes/scene.js
+++ src/scenes/scene.js
@@ -1,16 +1,8 @@
 import * as THREE from 'three'
-import { CONFIG } from '../config/index.js'
 
 // 创建场景
 export const createScene = () => {
   const scene = new THREE.Scene()
-
-  // 添加环境光
-  const ambientLight = new THREE.AmbientLight(
-    CONFIG.scene.ambientLight.color,
-    CONFIG.scene.ambientLight.intensity
-  )
-  scene.add(ambientLight)
 
   return scene
 }
